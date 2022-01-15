127eea14876ce357b3c76bba9d084ded0a97a163
Ivan D Cuadro <34123131+idcuadro82@users.noreply.github.com>
2022-01-15 08:03:11 -0500

    :wrench: config: Adding Prettier rules

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  endOfLine: 'auto',
+  jsxSingleQuote: false,
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+};
